f46c5d9af5e39bb99c3553f590b60c5dd610b906
서 현식 <marco44467@gmail.com>
2022-05-06 21:33:25 +0900

    [change]

--- PP06/main.cpp
+++ PP06/main.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-//템플릿(제네릭)으로 생성
 template<typename T>
 T sum(T a, T b)
 {
@@ -14,18 +13,16 @@ T sum(T a, T b)
 int main()
 {
 	vector<int> a;
-	//8/8
+
 	cout << sum<double>(3.9, 4.9) << endl;
-	//백터값에 집어넣은 값이 없다
+
 	cout << a.size() << endl;
 
-	//백터 끝에 요소를 출력한다 1,2,4를 출력한다
 	a.push_back(1);
 	a.push_back(2);
 	a.push_back(4);
-	//a에 백터값3개를 넣었다
+
 	cout << a.size() << endl;
-	//a[0]번 즉 처음값을 출력한다 1이 출력된다
 	cout << "값 :" << a[0] << endl;
 
 	return 0;
